4589a97329ffb54bae141f2b7faa9e31a29bd053
BaoTran91 <BaoTranChBE@gmail.com>
2017-06-28 00:12:09 -0400

    Create Owner's Normal Hour Adjustment Page

---
+++ Trang/BaseExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  BaseExtensions.swift
+//  Trang
+//
+//  Created by Bao Tran on 6/27/17.
+//  Copyright © 2017 Bao Tran. All rights reserved.
+//
+
+import Foundation
---
+++ Trang/SchedulingViewExt.swift
@@ -0,0 +1,9 @@
+//
+//  SchedulingViewExt.swift
+//  Trang
+//
+//  Created by Bao Tran on 6/27/17.
+//  Copyright © 2017 Bao Tran. All rights reserved.
+//
+
+import Foundation
