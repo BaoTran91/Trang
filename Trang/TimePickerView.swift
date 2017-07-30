f2df8f5783b453048d4bddba4152ccdbdb726a05
BaoTran91 <BaoTranChBE@gmail.com>
2017-07-30 11:40:38 -0400

    custom alert Nib Done

---
+++ Trang/TimePickerView.swift
@@ -0,0 +1,21 @@
+//
+//  TimePickerView.swift
+//  Trang
+//
+//  Created by Bao Tran on 7/26/17.
+//  Copyright © 2017 Bao Tran. All rights reserved.
+//
+
+import UIKit
+
+class TimePickerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
